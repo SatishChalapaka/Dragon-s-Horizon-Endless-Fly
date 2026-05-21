39b5e8d505f22fd4087dd5ef4236524a69839764
SatishChalapaka <102961102+SatishChalapaka@users.noreply.github.com>
2026-05-21 17:35:11 +0530

    21-05-2026 2

--- Assets/Malbers Animations/Common/Scripts/Editor/Menu/MalbersMenu.cs
+++ Assets/Malbers Animations/Common/Scripts/Editor/Menu/MalbersMenu.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿
+
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -6,79 +8,47 @@ namespace MalbersAnimations
 {
     public class MalbersMenu : EditorWindow
     {
-        const string URPL_Shader_Path2020 = "Assets/Malbers Animations/Common/Shaders/URP_Malbers Shaders 2020.unitypackage";
-        const string URPL_Shader_Path2021 = "Assets/Malbers Animations/Common/Shaders/URP_Malbers Shaders 2021.unitypackage";
-        const string HRPL20_Shader_Path = "Assets/Malbers Animations/Common/Shaders/HDRP_MalbersShaders 2020.unitypackage";
+        //const string URP14_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_URP_14.unitypackage";
+        const string URP14_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_URP_14.unitypackage";
+        // const string URP16_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_URP_16.unitypackage";
+        const string URP17_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_URP_17.unitypackage";
+
+        //  const string HRP15_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_HDRP_15.unitypackage";
+        const string HRP14_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_HDRP_15.unitypackage";
+        // const string HRP16_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_HDRP_16.unitypackage";
+        const string HRP17_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_HDRP_17.unitypackage";
+
+        const string D_Shader_Path = "Assets/Malbers Animations/Common/Shaders/Malbers_Standard.unitypackage";
 
 
-        [MenuItem("Tools/Malbers Animations/Malbers URP Shaders [Unity 2020]", false, 1)]
-        public static void UpgradeMaterialsURPL2020() => AssetDatabase.ImportPackage(URPL_Shader_Path2020, true);
+        [MenuItem("Tools/Malbers Animations/Malbers URP 17 Shaders (Unity6) ", false, 2)]
+        public static void UpgradeMaterialsURP_17() => AssetDatabase.ImportPackage(URP17_Shader_Path, true);
+
+        [MenuItem("Tools/Malbers Animations/Malbers URP 14 Shaders (Unity 2022.3 LTS) ", false, 1)]
+        public static void UpgradeMaterialsURP_14() => AssetDatabase.ImportPackage(URP14_Shader_Path, true);
+
+        //[MenuItem("Tools/Malbers Animations/Malbers URP 16 Shaders", false, 2)]
+        //public static void UpgradeMaterialsURP_16() => AssetDatabase.ImportPackage(URP16_Shader_Path, true);
+
+        [MenuItem("Tools/Malbers Animations/Malbers HDRP 14 Shaders (Unity 2022.3 LTS) ", false, 3)]
+        public static void UpgradeMaterialsHDRP_14() => AssetDatabase.ImportPackage(HRP14_Shader_Path, true);
+
+        [MenuItem("Tools/Malbers Animations/Malbers HDRP 17 Shaders (Unity6)", false, 3)]
+        public static void UpgradeMaterialsHDRP_17() => AssetDatabase.ImportPackage(HRP17_Shader_Path, true);
 
 
-
-        [MenuItem("Tools/Malbers Animations/Malbers URP Shaders [Unity 2021]", false, 1)]
-        public static void UpgradeMaterialsURPL2021() => AssetDatabase.ImportPackage(URPL_Shader_Path2021, true);
-
-
-
-        [MenuItem("Tools/Malbers Animations/Malbers HDRP Shaders", false, 1)]
-        public static void UpgradeMaterialsHRPL2020() => AssetDatabase.ImportPackage(HRPL20_Shader_Path, true);
-
-
-
-        [MenuItem("Tools/Malbers Animations/Create Test Scene", false, 100)]
-        public static void CreateTestPlane()
-        {
-            var all = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects().ToList();
-
-            var mainCam = all.Find(x => x.name == "Main Camera");
-            if (mainCam)
-            { DestroyImmediate(mainCam); }
-
-
-            var TestPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            TestPlane.transform.localScale = new Vector3(20, 1, 20);
-            TestPlane.GetComponent<MeshRenderer>().sharedMaterial = AssetDatabase.LoadAssetAtPath("Assets/Malbers Animations/Common/Shaders/Ground_20.mat", typeof(Material)) as Material;
-            TestPlane.isStatic = true;
-
-            var BrainCam = AssetDatabase.LoadAssetAtPath("Assets/Malbers Animations/Common/Cinemachine/CM Brain.prefab", typeof(GameObject)) as GameObject;
-            var CMFreeLook = AssetDatabase.LoadAssetAtPath("Assets/Malbers Animations/Common/Cinemachine/CM FreeLook Main.prefab", typeof(GameObject)) as GameObject;
-            var WolfLite = AssetDatabase.LoadAssetAtPath("Assets/Malbers Animations/Animal Controller/Wolf Lite/Wolf Lite.prefab", typeof(GameObject)) as GameObject;
-            var Steve = AssetDatabase.LoadAssetAtPath("Assets/Malbers Animations/Animal Controller/Human/Steve.prefab", typeof(GameObject)) as GameObject;
-
-            if (BrainCam) PrefabUtility.InstantiatePrefab(BrainCam);
-            if (CMFreeLook) PrefabUtility.InstantiatePrefab(CMFreeLook);
-            if (WolfLite) PrefabUtility.InstantiatePrefab(WolfLite);
-            if (Steve) PrefabUtility.InstantiatePrefab(Steve);
-        }
+        [MenuItem("Tools/Malbers Animations/Malbers Standard Shaders", false, 1)]
+        public static void UpgradeMaterialsStandard() => AssetDatabase.ImportPackage(D_Shader_Path, true);
 
 
 
-        [MenuItem("Tools/Malbers Animations/Integrations", false, 10)]
+
+        [MenuItem("Tools/Malbers Animations/Integrations", false, 600)]
         public static void OpenIntegrations() => Application.OpenURL("https://malbersanimations.gitbook.io/animal-controller/annex/integrations");
 
 
-        [MenuItem("Tools/Malbers Animations/Tools/Remove All MonoBehaviours from Selected", false, 100)]
-        public static void RemoveMono()
-        {
-            var allGo = Selection.gameObjects;
-
-            if (allGo != null)
-            {
-                foreach (var selected in allGo)
-                {
-                    var AllComponents = selected.GetComponentsInChildren<MonoBehaviour>(true);
-
-                    Debug.Log($"Removed {AllComponents.Length} from {selected}", selected);
-
-                    foreach (var comp in AllComponents)
-                    {
-                        var t = comp.gameObject;
-                        DestroyImmediate(comp);
-                        EditorUtility.SetDirty(t);
-                    }
-                }
-            }
-        }
+        [MenuItem("Tools/Malbers Animations/What's New", false, 600)]
+        public static void OpenWhatsNew() => Application.OpenURL("https://malbersanimations.gitbook.io/animal-controller/whats-new");
     }
 }
+#endif
